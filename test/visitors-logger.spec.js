a76ee6a7f5bdc43226c67fc4f91b771470441441
Tymoteusz Paszun <tpaszun@gmail.com>
2013-06-17 00:09:17 +0200

    Initial commit

---
+++ test/visitors-logger.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Tymoteusz Paszun
+ * Date: 17.06.2013
+ * Time: 00:04
+ */
+
+var visitorsLogger = require('../lib/visitors-logger');
+
+describe('visitors-logger', function() {
+
+});
